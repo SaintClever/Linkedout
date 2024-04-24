bca4814a3d4e759c16f0517bc92d7012d4bc80cc
Saint. Clever <parchment@gmx.com>
2024-04-23 23:08:55 -0400

    Initialize neon.tech psql db

--- database/database.py
+++ database/database.py
@@ -4,18 +4,39 @@ import psycopg2
 
 load_dotenv(".env")
 
-host: str = os.getenv("HOST")
-database: str = os.getenv("DATABASE")
-user: str = os.getenv("USER")
-password: str = os.getenv("PASSWORD")
+# host: str = os.getenv("HOST")
+# database: str = os.getenv("DATABASE")
+# user: str = os.getenv("USER")
+# password: str = os.getenv("PASSWORD")
+
+
+# def connect_to_db():
+#     conn = psycopg2.connect(
+#         host=host,
+#         database=database,
+#         user=user,
+#         password=password,
+#     )
+#     return conn
+
+
+# db_connection = connect_to_db()
+
+# neon.tech psql
+pguser: str = os.getenv("PGUSER")
+pghost: str = os.getenv("PGHOST")
+pgdatabase: str = os.getenv("PGDATABASE")
+pgpassword: str = os.getenv("PGPASSWORD")
+# pgport: str = os.getenv("PGPORT")
 
 
 def connect_to_db():
     conn = psycopg2.connect(
-        host=host,
-        database=database,
-        user=user,
-        password=password,
+        user=pguser,
+        host=pghost,
+        database=pgdatabase,
+        password=pgpassword,
+        # port=pgport,
     )
     return conn
 
