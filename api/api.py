55b146296f9118dc380e17c121a5369997d06ee5
Saint. Clever <parchment@gmx.com>
2024-04-25 17:54:26 -0400

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,9 +1,29 @@
+from job_boards.jobs_to_db import insert_job_data
+from database.database import db_connection
 from flask import Flask, request, jsonify
-import psycopg2
+
 
 app = Flask(__name__)
 
 
+# {
+#     "company_name": "linkedout",
+#     "job_title": "Python Developer",
+#     "job_href": "https://www.linkedout.com/",
+#     "location": "Remote",
+#     "currency": "USD",
+#     "starting_salary": 100000,
+#     "max_salary": 750000
+# }
+
+
 @app.route("/save_data", methods=["POST"])
 def save_data():
-    return ...
+    data = request.json
+    print(data)
+    insert_job_data(db_connection, data)
+    return jsonify({"message": "Data saved successfully"}), 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
